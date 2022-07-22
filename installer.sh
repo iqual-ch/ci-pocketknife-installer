534ebd97d0493e87fba454d73f78aed64aa71a14
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2022-07-22 18:17:50 +0200

    Better formatting and skipping compression

--- installer.sh
+++ installer.sh
@@ -11,7 +11,7 @@
 
 CI_VR_TOOLS_DOCKER_TAG=1.0.2
 CI_VR_TOOLS_DOCKER_CACHED_FOLDER=docker-cache
-CI_VR_TOOLS_DOCKER_CACHED_IMAGE_BZIP=$CI_VR_TOOLS_DOCKER_CACHED_FOLDER/ci-pocketknife.tar.bz2
+CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR=$CI_VR_TOOLS_DOCKER_CACHED_FOLDER/ci-pocketknife.tgz
 CI_VR_WORKING_FOLDER="${CI_VR_WORKING_FOLDER:-/tmp/ci}"
 CI_VR_DOCKER_NETWORK="${CI_VR_DOCKER_NETWORK:-host}"
 
@@ -19,13 +19,11 @@ CI_VR_DOCKER_NETWORK="${CI_VR_DOCKER_NETWORK:-host}"
 mkdir -p $CI_VR_WORKING_FOLDER/bin
 
 # Get the cached dockerized application.
-( [ -f $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_BZIP ] && docker load --input CI_VR_TOOLS_DOCKER_CACHED_IMAGE_BZIP ) \
-||
+[[ -f $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR && $(docker load --input $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR) ]] || \
 # Get the dockerized application and cache it.
-( [ -d $CI_VR_TOOLS_DOCKER_CACHED_FOLDER ] && docker pull iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG && docker save iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG | bzip2 > $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_BZIP )
-||
+[[ -d $CI_VR_TOOLS_DOCKER_CACHED_FOLDER && $(docker pull iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG && docker save iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG > $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR) ]] || \
 # Assuming no cache is enabled.
-( docker pull iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG )
+docker pull iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG
 
 # Create a cli shortcut for the glue commands
 cat << EOF > ${CI_VR_WORKING_FOLDER}/bin/g
