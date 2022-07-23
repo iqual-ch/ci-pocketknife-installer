4b840c245d192c58ead352c252118bb1f39809ed
Stefanos Petrakis <stefanos.petrakis+github@gmail.com>
2022-07-23 12:03:42 +0300

    Caching docker pulls (#4)
    * Caching docker pulls
    
    * Trying the standard gh actions cache, to avoid caching all docker images
    
    * Better formatting and skipping compression
    
    * Create the cache folder
    
    * Messing with the cache key some (to make sure we are getting clean results)
    
    * Adding some more dbing info
    
    * Using an absolute path
    
    * Switching tmp with github.action_path
    
    * Skip explicitely creating the docker-cache folder
    And drop use of the github.action_path since "Relative pathing '.' and '..' is not allowed."
    
    * We need the cache path to be present
    
    * It's TAR we want
    
    * Commenting
    
    Co-authored-by: Stefanos Petrakis <stefanos.petrakis@gmail.com>

--- installer.sh
+++ installer.sh
@@ -10,13 +10,19 @@
 # + CI_VR_DOCKER_NETWORK=host
 
 CI_VR_TOOLS_DOCKER_TAG=1.0.2
+CI_VR_TOOLS_DOCKER_CACHED_FOLDER=/tmp/docker-cache
+CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR=$CI_VR_TOOLS_DOCKER_CACHED_FOLDER/ci-pocketknife.tar
 CI_VR_WORKING_FOLDER="${CI_VR_WORKING_FOLDER:-/tmp/ci}"
 CI_VR_DOCKER_NETWORK="${CI_VR_DOCKER_NETWORK:-host}"
 
 # Create working directory.
 mkdir -p $CI_VR_WORKING_FOLDER/bin
 
-# Get the dockerized application
+# Load the docker image from the cache ...
+[[ -f $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR && $(docker load --input $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR) ]] || \
+# or, pull the docker image and cache it ...
+[[ -d $CI_VR_TOOLS_DOCKER_CACHED_FOLDER && $(docker pull iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG && docker save iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG > $CI_VR_TOOLS_DOCKER_CACHED_IMAGE_TAR) ]] || \
+# or, assuming no cache is enabled, simply pull the image.
 docker pull iqualch/ci-pocketknife:$CI_VR_TOOLS_DOCKER_TAG
 
 # Create a cli shortcut for the glue commands
